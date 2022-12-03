1dd6dfb5d3ff1d14067f869fd59dc1a692c4b7a6
Adrian Heil <a.heil@outlook.com>
2022-12-03 11:51:28 +0100

    add some additional extensions that may be useful later

--- AdventOfCode/extensions.swift
+++ AdventOfCode/extensions.swift
@@ -67,3 +67,33 @@ extension StringProtocol {
         return self == self.lowercased()
     }
 }
+
+extension String {
+    func toDate(withFormat format: String = "yyyy-MM-dd HH:mm:ss") -> Date?{
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Tehran")
+        dateFormatter.locale = Locale(identifier: "fa-IR")
+        dateFormatter.calendar = Calendar(identifier: .gregorian)
+        dateFormatter.dateFormat = format
+        let date = dateFormatter.date(from: self)
+        
+        return date
+    }
+}
+
+extension NSRegularExpression {
+    convenience init(_ pattern: String) {
+        do {
+            try self.init(pattern: pattern)
+        } catch {
+            preconditionFailure("Illegal regular expression: \(pattern).")
+        }
+    }
+}
+
+extension NSRegularExpression {
+    func matches(_ string: String) -> Bool {
+        let range = NSRange(location: 0, length: string.utf16.count)
+        return firstMatch(in: string, options: [], range: range) != nil
+    }
+}
