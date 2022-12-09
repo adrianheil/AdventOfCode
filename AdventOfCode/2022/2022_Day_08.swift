d6e36b50defa5dd831d9adc8909458e3dd59432b
Adrian Heil <a.heil@outlook.com>
2022-12-09 17:34:47 +0100

    cleanup 2022 Day 8 Part 1

--- AdventOfCode/2022/2022_Day_08.swift
+++ AdventOfCode/2022/2022_Day_08.swift
@@ -16,74 +16,45 @@ func Day08_2022(data: [String.SubSequence]) {
 }
 
 private func Part_1(input: [String]) -> Int {
-    var visibleTree = 0
-    
+    var visibleTrees = 0
     for i in 0..<input.count {
-    treeRow: for j in 0..<input[i].count {
-        //MARK: get outer trees, all are visible from at least one side
-        if i == 0 || j == 0 || i == input.count-1 || j == input.count-1 {
-            visibleTree += 1
-            continue treeRow
-        }
-        //MARK: get interior trees
-        // visible from left
-        for horizontal in 0..<j {
-            // if any tree is larger, the tree is not visible from that side
-            if input[i][j] <= input[i][horizontal] {
-                break
+        for j in 0..<input[i].count {
+            if i == 0 || j == 0 || i == input.count-1 || j == input.count-1 {
+                visibleTrees+=1
+                continue
             }
-            
-            if horizontal == j-1 {
-                // if we have made it this far, the tree is visible
-                visibleTree += 1
-                continue treeRow
+            var isVisible = [true, true, true, true]
+            for horizontal in 0..<j {
+                if input[i][j] <= input[i][horizontal] {
+                    isVisible[0] = false
+                    break
+                }
             }
-        }
-        // visible from right
-        for horizontal in j+1..<input[i].count {
-            // if any tree is larger, the tree is not visible from that side
-            if input[i][j] <= input[i][horizontal] {
-                break
+            for horizontal in j+1..<input[i].count {
+                // if any tree is larger, the tree is not visible from that side
+                if input[i][j] <= input[i][horizontal] {
+                    isVisible[1] = false
+                    break
+                }
             }
-            
-            if horizontal == input[i].count-1 {
-                // if we have made it this far, the tree is visible
-                visibleTree += 1
-                continue treeRow
+            for vertical in 0..<i {
+                if input[i][j] <= input[vertical][j] {
+                    isVisible[2] = false
+                    break
+                }
             }
-        }
-        
-        // visible from top
-        for vertical in 0..<i {
-            // if any tree is larger, the tree is not visible from that side
-            if input[i][j] <= input[vertical][j] {
-                break
+            for vertical in i+1..<input.count {
+                if input[i][j] <= input[vertical][j] {
+                    isVisible[3] = false
+                    break
+                }
             }
-            
-            if vertical == i-1 {
-                // if we have made it this far, the tree is visible
-                visibleTree += 1
-                continue treeRow
-            }
-        }
-        
-        // visible from bottom
-        for vertical in i+1..<input.count {
-            // if any tree is larger, the tree is not visible from that side
-            // no need to check the remaining trees
-            if input[i][j] <= input[vertical][j] {
-                break
-            }
-            
-            if vertical == input.count-1 {
-                // if we have made it this far, the tree is visible
-                visibleTree += 1
-                continue treeRow
+            if isVisible.contains(true) {
+                visibleTrees+=1
             }
         }
     }
-    }
-    return visibleTree
+    return visibleTrees
 }
 
 private func Part_2(input: [String]) -> Int {
